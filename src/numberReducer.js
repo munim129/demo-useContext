32b62faf7ad4071ea8222db74f93a2faf095fc33
munim129 <munim129@users.noreply.github.com>
2020-06-15 22:39:48 +0500

    deleted use context

--- src/numberReducer.js
+++ src/numberReducer.js
@@ -1,9 +1,9 @@
 const numberReducer = (state, action) => {
-    switch (action.type) {
-      case 'INCREMENT':
-        return state + action.val
-      case 'DECREMENT':
-        return state - action.val
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + action.val
+    case 'DECREMENT':
+      return state - action.val
+  }
 }
 export default numberReducer;
